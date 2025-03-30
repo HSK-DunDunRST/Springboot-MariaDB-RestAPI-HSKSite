4ea3b550a97519794060e113d7d764b559067343
HSK-DunDunRST <gon5073@naver.com>
2025-03-31 00:32:03 +0900

    프로젝트 생성(기본값)

---
+++ src/main/java/kr/co/ipdisk/dundunhsk/apiboard/ApiboardApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ipdisk.dundunhsk.apiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/ipdisk/dundunhsk/apiboard/ApiboardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ipdisk.dundunhsk.apiboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
